e79648ea021b1dd0954cb342d98f402c845b85e0
regnaio <gbzregna@gmail.com>
2020-06-14 20:34:41 -0700

    delete

--- example/client/webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mode: 'development', // change to 'production' for minified bundle.js
-  entry: './js/example/client/ts/app.js',
-  output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js'
-  }
-}
--- example/server/js/src/server/ts/iceServers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.iceServers = [
-    { urls: 'stun:stun1.l.google.com:19302' },
-    { urls: 'stun:stun2.l.google.com:19302' },
-    { urls: 'stun:stun3.l.google.com:19302' },
-    { urls: 'stun:stun4.l.google.com:19302' }
-];
